f03c7c7c7b2b4ead33b59ae63947ba7ff56e4f6a
ramasuke <90756667+ramasuke@users.noreply.github.com>
2025-08-28 07:29:58 +0900

    add: quizUIと多少の演出を追加
    演出の追加項目は、何秒間か止まる程度とUIの変更

--- Assets/NiceBody/Quiz/QuizStatus.cs
+++ Assets/NiceBody/Quiz/QuizStatus.cs
@@ -7,12 +7,15 @@ using UnityEngine;
 public sealed class QuizStatus : ScriptableObject
 {
     [SerializeField] string topicText = "";
+    [SerializeField] Sprite dateSprite;
     [SerializeField] List<AnswerCandidate> answerCandiates_ = new();
     [SerializeField] int correctAnswerIndex_ = 0;
 
     public string TopicText => topicText;
+    public Sprite DateSprite => dateSprite;
     public List<AnswerCandidate> AnswerCandiates => answerCandiates_;
     public int CorrectAnswerIndex => correctAnswerIndex_;
+
 
     [Serializable]
     public sealed class AnswerCandidate
--- Assets/NiceBody/Quiz/QuizSystem.cs
+++ Assets/NiceBody/Quiz/QuizSystem.cs
@@ -7,45 +7,85 @@ using UnityEngine.UI;
 public sealed class QuizSystem : MonoBehaviour
 {
     [Header("Model")]
-    [SerializeField] QuizStatusGroup quizStatusGroup;
-    [SerializeField] List<QuizStatus> appearedQuizzes = new();
-    [SerializeField] int appearQuizCount = 10;
+    [SerializeField] QuizStatusGroup quizStatusGroup_;
+    [SerializeField] List<QuizStatus> appearedQuizzes_ = new();
+    [SerializeField] int maxAppearQuizCount_ = 10;
+    [SerializeField] int appearQuizCount_ = 0;
 
     [Header("View")]
+    [SerializeField] Canvas quizCanvas;
+    [SerializeField] Image dateImageBox_;
     [SerializeField] AnswerCnadiateUiGroup answerCandiateUiGroup_;
-    [SerializeField] Text topicTextBox;
+    [SerializeField] Text topicTextBox_;
+
+    [Header("answerCharactorView")]
+    [SerializeField] Image answerCharactorImage_;
+    [SerializeField] Sprite correctAnswerCharactorSprite_;
+    [SerializeField] Sprite wrongAnswerCharactorSprite_;
+    [SerializeField] Sprite stayAnswerCharactorSprite_;
+
+    [Header("ResultView")]
+    [SerializeField] Canvas resultCanvas;
 
 
     private void Start()
     {
-        OnPlay();
+        OnAskQuiz();
     }
 
-    private void OnPlay()
+    private void OnAskQuiz()
     {
-        var quiz = quizStatusGroup.GetRandomQuizStatus(appearedQuizzes);
-        appearedQuizzes.Add(quiz);
+        var quiz = quizStatusGroup_.GetRandomQuizStatus(appearedQuizzes_);
+        appearQuizCount_++;
+        appearedQuizzes_.Add(quiz);
 
+        answerCharactorImage_.sprite = stayAnswerCharactorSprite_;
+        dateImageBox_.sprite = quiz.DateSprite;
+        topicTextBox_.text = quiz.TopicText;
 
-        topicTextBox.text = quiz.TopicText;
         var zipped = quiz.AnswerCandiates.Zip(answerCandiateUiGroup_.Get, (answer, ui) => (answer, ui));
 
         foreach (var (answer, ui) in zipped)
         {
             ui.TextBox.text = answer.Text;
-            ui.Button.onClick.AddListener(() =>
-            {
-                if (quiz.CorrectAnswerIndex == quiz.AnswerCandiates.IndexOf(answer))
-                {
-                    Debug.Log("Correct!");
-                }
-                else
-                {
-                    Debug.Log("Wrong...");
-                }
-                zipped.ToList().ForEach(t => t.ui.Button.onClick.RemoveAllListeners());
-                OnPlay();
-            });
+            ui.Button.onClick.AddListener(() => StartCoroutine(OnAnswerSelect(answer)));
         }
+
+        IEnumerator OnAnswerSelect(QuizStatus.AnswerCandidate answer)
+        {
+            zipped.ToList().ForEach(t => t.ui.Button.onClick.RemoveAllListeners());
+
+            if (quiz.CorrectAnswerIndex == quiz.AnswerCandiates.IndexOf(answer))
+            {
+                answerCharactorImage_.sprite = correctAnswerCharactorSprite_;
+                Debug.Log("Correct!");
+            }
+            else
+            {
+                answerCharactorImage_.sprite = wrongAnswerCharactorSprite_;
+                Debug.Log("Wrong...");
+            }
+
+            yield return new WaitForSeconds(1.0f);
+
+            if (maxAppearQuizCount_ > appearQuizCount_)
+            {   
+                OnAskQuiz();
+            }
+            else
+            {
+                OnMigrateResult();
+            }
+        }
+    }
+
+    private IEnumerator OnMigrateResult()
+    {
+        yield return new WaitForSeconds(0.8f);
+
+        quizCanvas.enabled = false;
+        resultCanvas.enabled = true;
+
+        
     }
 }
