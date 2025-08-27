172a50aa230ce9b60ff8eed7ec5e172ba132580b
kuroda50 <144975768+kuroda50@users.noreply.github.com>
2025-08-27 20:25:34 +0900

    音楽を切り替える処理を追加

--- Assets/kuroriku/Scripts/BGMManager.cs
+++ Assets/kuroriku/Scripts/BGMManager.cs
@@ -9,7 +9,7 @@ public class BGMManager : MonoBehaviour
     public static BGMManager instance;
 
     [SerializeField] AudioSource bgmAudioSource;
-
+    [SerializeField] public AudioClip[] bgmClips; // 再生したいBGMのリスト
     void Awake()
     {
         if (instance == null)
@@ -20,6 +20,29 @@ public class BGMManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+
+    public void PlayBGM(int index)
+    {
+        // 指定された番号がリストの範囲内かチェック
+        if (index < 0 || index >= bgmClips.Length)
+        {
+            Debug.LogWarning("BGMの指定番号が範囲外です。");
+            return;
+        }
+
+        // 再生するBGMクリップを取得
+        AudioClip newClip = bgmClips[index];
+
+        // もし、すでに同じBGMが流れていたら何もしない
+        if (bgmAudioSource.clip == newClip)
+        {
+            return;
+        }
+
+        // BGMを差し替えて再生
+        bgmAudioSource.clip = newClip;
+        bgmAudioSource.Play();
     }
 
     public void SetBGMVolume(float volume)
