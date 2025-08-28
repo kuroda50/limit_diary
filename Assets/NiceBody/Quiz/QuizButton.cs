603259c483625cbb9f163aafe83b0b8aa96d5491
ramasuke <90756667+ramasuke@users.noreply.github.com>
2025-08-28 14:37:04 +0900

    add: 全般の統合

---
+++ Assets/NiceBody/Quiz/QuizButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public sealed class QuizButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    [SerializeField] Sprite idleSprite;
+    [SerializeField] Sprite pusshingSprite;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        GetComponent<Image>().sprite = pusshingSprite;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        GetComponent<Image>().sprite = idleSprite; 
+    }
+}
---
+++ Assets/NiceBody/Title/OpenSettingsButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public sealed class OpenSettingsButton : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        SceneManager.LoadScene("Settings");
+    }
+}
