09baa569e73bf89bfc74cee9549f357cc4b079f9
ramasuke <90756667+ramasuke@users.noreply.github.com>
2025-08-29 20:30:55 +0900

    add: resultの次へボタンを追加

--- Assets/kuroriku/Scripts/BackButton.cs
+++ Assets/kuroriku/Scripts/BackButton.cs
@@ -1,11 +1,10 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class BuckButton : MonoBehaviour
 {
     public void OnClick(string sceneName)
     {
-        Debug.Log(sceneName + "シーンに移動する処理を実装してください");
         SceneManager.LoadScene(sceneName);
     }
 }
